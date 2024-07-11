0a159bc499a20b67abdf159bdebc97a1787c9ca1
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-07-11 14:11:41 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    
+    // Target the paragraph element with id="text"
+    const textElement = document.getElementById('text');
+    
+    // Replace the text with "This is really cool!"
+    textElement.textContent = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  
